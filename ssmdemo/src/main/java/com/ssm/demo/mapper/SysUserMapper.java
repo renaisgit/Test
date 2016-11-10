944cd7940e857f1f657324840fe5207fc6bffce1
renaisgit <314943315@qq.com>
2016-11-10 14:10:50 +0800

    更新

---
+++ ssmdemo/src/main/java/com/ssm/demo/mapper/SysUserMapper.java
@@ -0,0 +1,8 @@
+package com.ssm.demo.mapper;
+
+import com.ssm.demo.common.mapper.BaseMapper;
+import com.ssm.demo.models.SysUser;
+
+public interface SysUserMapper extends BaseMapper<SysUser, String>{
+	
+}
---
+++ ssmdemo/src/main/java/com/ssm/demo/models/SysUser.java
@@ -0,0 +1,58 @@
+package com.ssm.demo.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SysUser implements Serializable {
+    private String id;
+
+    private String userName;
+
+    private String userPasswd;
+
+    private Date createDate;
+
+    private Date updateDate;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserPasswd() {
+        return userPasswd;
+    }
+
+    public void setUserPasswd(String userPasswd) {
+        this.userPasswd = userPasswd == null ? null : userPasswd.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
